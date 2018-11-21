09d4b6e7d0bcebf99781bad4ec800ba202eccea1
venusseo127 <venusseo127@gmail.com>
2018-11-21 09:00:15 +0800

    Create welcome.php

---
+++ controllers/welcome.php
@@ -0,0 +1,113 @@
+<?php
+class Welcome extends CI_Controller {
+	function __construct()
+	{
+		parent::__construct();
+        include_once($this->config->item('dynamodb').'aws-autoloader.php');
+		$this->load->helper('url');
+		$this->load->helper('date');
+		$this->load->model('Users_model','',TRUE);
+	}
+	function index()
+	{
+		$data = array(
+			'members' => array()
+		);
+        $room2 = array();
+        
+		if (!$this->Users_model->table_exists()){
+			$this->load->view('no_table', $data);
+		} else {
+
+			$rooms = $this->Users_model->getAll(); // Find all members, limit by 5
+
+            usort($rooms, function ($a, $b) { return ($a['roomid'] > $b['roomid']); });
+
+            $data['rooms'] = $rooms;
+		 $this->load->view('welcome_message', $data);	
+		}
+       
+	}
+    function view($memberid)
+	{
+        
+        //echo $memberid;
+		$roomslist = $this->Users_model->getByID($memberid); // Find member details
+        //var_dump($roomslist);
+        $data2['room1']=$roomslist;
+
+		$this->load->view('view', $data2);
+	}
+	
+	function edit($roomid=0)
+	{
+        echo $roomid;
+        //return $memberid;
+		$roomslist = $this->Users_model->getByID($roomid); // Find member details
+		$data['room1']=$roomslist;
+		$data['inserted'] = FALSE;
+		// If form submitted
+		if($this->input->post('edit'))
+		{ 
+			// add new member into array
+			$room = array(
+				'roomid' => (string)$roomid,
+                'title' => $this->input->post('title'),
+				'owner' => $this->input->post('owner'),
+				'background' => $this->input->post('background')
+			);
+            //print_r($room);
+			$this->Users_model->save($room); // Insert the member
+			
+			$data2['inserted'] = TRUE;
+            $roomslist2 = $this->Users_model->getByID($roomid); // Find member details
+		    $data2['room1']=$roomslist2;
+			$this->load->view('view', $data2);
+		}else{
+		
+		    $this->load->view('edit', $data); // Load the form
+        }
+	}
+	function add()
+	{
+		$data = array();
+		$data['inserted'] = FALSE;
+		
+		// If form submitted
+		if($this->input->post('add'))
+		{
+			// add new member into array
+			$room = array(
+                'title' => $this->input->post('title'),
+				'owner' => $this->input->post('owner'),
+				'background' => $this->input->post('background')
+			);
+			$addroom = $this->Users_model->save($room); // Insert the member
+
+			$data2['inserted'] = TRUE;
+            $roomslist2 = $this->Users_model->getByID($addroom); // Find member details
+		    $data2['room1']=$roomslist2;
+			$this->load->view('view', $data2);
+		}else{
+		
+		    $this->load->view('add', $data); // Load the form
+        }
+	}
+	
+	
+	function delete($memberid)
+	{
+		$members = $this->Users_model->deleteById($memberid); // Find member details
+		//redirect('/', 'refresh');
+        if($members){
+            $rooms = $this->Users_model->getAll(); 
+            usort($rooms, function ($a, $b) { return ($a['roomid'] > $b['roomid']); });
+            $data['rooms'] = $rooms;
+            $this->load->view('welcome_message', $data);
+        }else{
+            print_r($members);
+        }
+	}
+}
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
